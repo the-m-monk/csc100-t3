4aad4f331e840afd87513b67e6c453b2f5d3b8f4
the-m-monk <themmonk@gmail.com>
2026-09-08 20:36:42 +1000

    Begin working on raysim

---
+++ src/csc100_t3/__init__.py
@@ -0,0 +1,14 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(prog="csc100_t3")
+    sub = parser.add_subparsers(dest="command", required=True)
+
+    sub.add_parser("emptybox")
+
+    args = parser.parse_args()
+
+    match args.command:
+        case "emptybox":
+            from csc100_t3.raysim.emptybox import run
+            run()
