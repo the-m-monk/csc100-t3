4cf9f86bcdd8882b9feab0f4df0dfda9afd0d616
the-m-monk <themmonk1@gmail.com>
2026-09-14 23:39:00 +1000

    model scaffold

---
+++ src/csc100_t3/model/__init__.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+import numpy as np
+
+class DogModelAction(Enum):
+    FORWARD = 1
+    BACKWARD = 2
+    LEFT = 3
+    RIGHT = 4
+    TUNNEL = 5
+    RAMP = 6
+    FINISHED = 7
+
+FB_DIMENSIONS = (480, 640, 3)
+
+class DogModel():
+    def forward(self, fb: np.ndarray) -> DogModelAction:
+        if isinstance(np.ndarray, fb):
+            raise TypeError("fb must be a np.ndarray")
+
+        if fb.shape != FB_DIMENSIONS:
+            raise ValueError(f"fb.shape != {FB_DIMENSIONS}")
+
+        if fb.dtype != np.uint8:
+            raise TypeError("fb.dtype != np.uint8")    
+        
+        return DogModelAction.FINISHED
