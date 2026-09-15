10d3d99aaf2c80bf08730dd652843b767d84f5a7
the-m-monk <themmonk1@gmail.com>
2026-09-15 19:34:52 +1000

    Intial mujoco scene + temp tile texture

--- src/csc100_t3/mjsim/emptybox.py
+++ src/csc100_t3/mjsim/emptybox.py
@@ -1,2 +1,28 @@
+import time
+from pathlib import Path
+
+import mujoco
+import mujoco.viewer
+
+SCENE_PATH = Path(__file__).resolve().parents[3] / "scene" / "main.xml"
+
 def run():
-    print("hello world")
+    scene = mujoco.MjModel.from_xml_path(str(SCENE_PATH))
+
+    state = mujoco.MjData(scene)
+
+    with mujoco.viewer.launch_passive(
+        scene,
+        state,
+        show_left_ui=False,
+        show_right_ui=False,
+    ) as viewer:
+        with viewer.lock():
+            mujoco.mjv_defaultFreeCamera(scene, viewer.cam)
+
+        while viewer.is_running():
+            mujoco.mj_step(scene, state)
+
+            viewer.sync()
+
+            time.sleep(scene.opt.timestep)
